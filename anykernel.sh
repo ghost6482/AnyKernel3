6093303fc4501569703f2ab748e95dcb49c58192
Alex Winkowski <dereference23@outlook.com>
2021-02-24 19:22:11 +0300

    ak3: Make it universal

--- anykernel.sh
+++ anykernel.sh
@@ -4,19 +4,18 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=
-do.devicecheck=1
+kernel.string=Positron Kernel
+do.devicecheck=0
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
